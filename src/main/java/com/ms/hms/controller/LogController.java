a4219fcec911c6bad222871a07b80121e7ddbecf
zhaojianhua <1161223922@qq.com>
2021-11-10 09:59:40 +0800

    查询日志功能

---
+++ src/main/java/com/ms/hms/controller/LogController.java
@@ -0,0 +1,35 @@
+package com.ms.hms.controller;
+
+import com.ms.hms.aop.Log;
+import com.ms.hms.common.result.R;
+import com.ms.hms.exception.ExceptionCode;
+import com.ms.hms.exception.ResultHttpCode;
+import com.ms.hms.exception.ServiceException;
+import com.ms.hms.service.SysLogService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+@RestController
+@RequestMapping(value = "/log")
+public class LogController {
+    @Autowired
+    private SysLogService sysLogService;
+
+    @Log(value = "查询日志")
+    @GetMapping(value = "/sysLog")
+    public R getSysLogList(@RequestParam(defaultValue = "1") Integer pageNo,@RequestParam(defaultValue = "10") Integer pageSize,@RequestParam("search") String search) throws UnsupportedEncodingException {
+        if (pageNo==null || pageSize ==null || StringUtils.isBlank(search)){
+            throw new ServiceException(ExceptionCode.PARAMTER_ERROR);
+        }
+//        String searchStr = URLDecoder.decode(search,"utf8");
+//        System.out.println(searchStr);
+        return sysLogService.getSysLog(pageNo,pageSize,search);
+    }
+}
